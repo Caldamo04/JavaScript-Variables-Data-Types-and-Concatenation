78091f9c7960360849e9a36512d5f6055c2260cf
Caldamo04 <146355158+Caldamo04@users.noreply.github.com>
2024-05-17 08:56:21 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,34 @@
+
+let webWeaver = "web weaver";
+let alchemistOfTheBrowser = "alchemist of the browser";
+let cssCloak = "CSS cloak";
+let javaScriptSpells = "JavaScript spells";
+let debuggingSpectacles = "debugging spectacles";
+let responsiveSea = "responsive sea";
+let seoWhisperer = "SEO whisperer";
+let git = "Git";
+let webDeveloper = "web developer";
+
+
+let sentence1 = "War is a " + webWeaver + " with dreams and pearls.";
+let sentence2 = "As an " + alchemistOfTheBrowser + ", War conjures pages from thin air.";
+let sentence3 = "With his " + cssCloak + ", he styles elements with flair and grace.";
+let sentence4 = "War summons " + javaScriptSpells + ", making interactivity bloom at his will.";
+let sentence5 = "Wearing his " + debuggingSpectacles + ", War tames wild loops and elusive variables.";
+let sentence6 = "On the " + responsiveSea + ", War adapts layouts with fluidity.";
+let sentence7 = "As an " + seoWhisperer + ", he crafts pathways to digital wanderers.";
+let sentence8 = "Using " + git + ", War manages version control with mystical precision.";
+let sentence9 = "War is a " + webDeveloper + ", weaving dreams into the digital matrix.";
+
+
+let contentDiv = document.getElementById("content");
+
+contentDiv.innerHTML += "<p>" + sentence1 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence2 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence3 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence4 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence5 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence6 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence7 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence8 + "</p>";
+contentDiv.innerHTML += "<p>" + sentence9 + "</p>";
